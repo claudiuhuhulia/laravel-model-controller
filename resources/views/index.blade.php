629dcfc991cf102b7cc50f86e987c7835a24658a
cloudhuh <claudiu.huhulia@gmail.com>
2023-08-22 17:18:32 +0200

    db iteraction

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,21 @@
 @section('main')
     <div class="container">
         <h1>I NOSTRI FILM</h1>
-        <div class="row row-cols-2">
+        <div class="row row-cols-3">
             @foreach ($movies as $movie)
-                <div class="col">
-                    <h3>Titolo: {{ $movie->title }}</h3>
+                <div class="col g-3">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Titolo: {{ $movie->title }}</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Titolo originale: {{ $movie->original_title }}
+                            </h6>
+                            <ul>
+                                <li>Nazionalità: {{ $movie->nationality }}</li>
+                                <li>Data d'uscita: {{ $movie->date }}</li>
+                                <li>Voto: {{ $movie->vote }}</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             @endforeach
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\HomeController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
